374df0604a417f2e6f84b552f69b1c72d01cdb99
Aaron Schlesinger <aaschles@microsoft.com>
2017-07-03 12:22:01 -0700

    Moving the TLS setup script into the chart

--- charts/catalog/aggregation-tls-setup.sh
+++ charts/catalog/aggregation-tls-setup.sh
@@ -14,6 +14,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+CUR=$(pwd)
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "Entering $DIR"
+cd $DIR
+
 export HELM_NAME=catalog
 export SVCCAT_NAMESPACE=catalog
 export SVCCAT_SERVICE_NAME=${HELM_NAME}-${SVCCAT_NAMESPACE}-apiserver
@@ -22,7 +27,7 @@ export CA_NAME=ca
 
 export ALT_NAMES="\"${SVCCAT_SERVICE_NAME}.${SVCCAT_NAMESPACE}\",\"${SVCCAT_SERVICE_NAME}.${SVCCAT_NAMESPACE}.svc"\"
 
-export SVCCAT_CA_SETUP=charts/catalog/svc-cat-ca.json
+export SVCCAT_CA_SETUP=svc-cat-ca.json
 cat > ${SVCCAT_CA_SETUP} << EOF
 {
     "hosts": [ ${ALT_NAMES} ],
@@ -43,19 +48,19 @@ cat > ${SVCCAT_CA_SETUP} << EOF
 EOF
 
 
-cfssl genkey --initca ${SVCCAT_CA_SETUP} | cfssljson -bare charts/catalog/${CA_NAME}
-# now the files 'charts/catalog/ca.csr, charts/catalog/ca-key.pem, and charts/catalog/ca.pem' exist
+cfssl genkey --initca ${SVCCAT_CA_SETUP} | cfssljson -bare ${CA_NAME}
+# now the files 'ca.csr, ca-key.pem, and ca.pem' exist
 
-export SVCCAT_CA_CERT=charts/catalog/${CA_NAME}.pem
-export SVCCAT_CA_KEY=charts/catalog/${CA_NAME}-key.pem
+export SVCCAT_CA_CERT=${CA_NAME}.pem
+export SVCCAT_CA_KEY=${CA_NAME}-key.pem
 
 export PURPOSE=server
-echo '{"signing":{"default":{"expiry":"43800h","usages":["signing","key encipherment","'${PURPOSE}'"]}}}' > "charts/catalog/${PURPOSE}-ca-config.json"
+echo '{"signing":{"default":{"expiry":"43800h","usages":["signing","key encipherment","'${PURPOSE}'"]}}}' > "${PURPOSE}-ca-config.json"
 
 echo '{"CN":"'${SVCCAT_SERVICE_NAME}'","hosts":['${ALT_NAMES}'],"key":{"algo":"rsa","size":2048}}' \
- | cfssl gencert -ca=${SVCCAT_CA_CERT} -ca-key=${SVCCAT_CA_KEY} -config=charts/catalog/server-ca-config.json - \
- | cfssljson -bare charts/catalog/apiserver
+ | cfssl gencert -ca=${SVCCAT_CA_CERT} -ca-key=${SVCCAT_CA_KEY} -config=server-ca-config.json - \
+ | cfssljson -bare apiserver
 
 export SC_SERVING_CA=${SVCCAT_CA_CERT}
-export SC_SERVING_CERT=charts/catalog/apiserver.pem
-export SC_SERVING_KEY=charts/catalog/apiserver-key.pem
+export SC_SERVING_CERT=apiserver.pem
+export SC_SERVING_KEY=apiserver-key.pem
--- docs/api-aggregation-setup.md
+++ docs/api-aggregation-setup.md
@@ -1,4 +1,3 @@
-
 # Setting up Service Catalog for API Aggregation in Kubernetes
 
 The aggregator is a server that sits in front of the core API
@@ -15,7 +14,7 @@ certificate bundle to the APIService apiregistration endpoint.
 
 For development purposes, it is convenient to use the existing CA
 automatically set up by the kubernetes development environment. The
-[script in contrib](../contrib/svc-cat-apiserver-aggregation-tls-setup.sh)
+[script in the catalog chart](../charts/catalog/aggregation-tls-setup.sh)
 generates a fresh CA and certificate setup, without using any existing
 kubernetes infrastructure CAs or certificates. This script should be
 `source`ed to define all of the variables it contains in the current
@@ -179,9 +178,11 @@ keys we just generated inline.
 
 ```
 helm install charts/catalog \
-    --name ${HELM_NAME} --namespace ${SVCCAT_NAMESPACE} \
+    --name ${HELM_NAME} \
+    --namespace ${SVCCAT_NAMESPACE} \
     --set apiserver.auth.enabled=true \
     --set useAggregator=true \
+    --set apiserver.insecure=false \
     --set apiserver.tls.caFileName=${SC_SERVING_CA} \
     --set apiserver.tls.certFileName=${SC_SERVING_CERT} \
     --set apiserver.tls.keyFileName=${SC_SERVING_KEY}
@@ -211,7 +212,7 @@ Before installing the helm chart, run the script in contrib by
 `source`ing it, to define all of the necessary variables.
 
 ```shell
-source /contrib/svc-cat-apiserver-aggregation-tls-setup.sh
+source charts/catalog/aggregation/tls-setup/sh
 ```
 
 To delete, all resources, execute the following:
